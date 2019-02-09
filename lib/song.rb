d51487f993a0cd190281f457e601fff71a0878a8
Yonnick Brown <25hour24@gmail.com>
2019-02-09 22:01:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,5 +2,12 @@ class Songs
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  
+
+  def initialize(name,artist,genre)
+    @name = mame
+    @artist = artist
+    @genre = genre
+
+    @@ count += 1
+  end
 end
